aa1fd124afc3a1e2ba5dac27717943778c880469
Rafael Souza Nascimento <83884395+RafaSouz@users.noreply.github.com>
2022-08-14 12:23:18 -0300

    Create const.py

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "172.31.20.174"
+CHAT_SERVER_PORT = 5001
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("172.31.29.218",5002), "Bob":("172.31.16.6",5002)}
